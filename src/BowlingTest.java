ba541d04bd94d906555d32938ff2cb126f5e27d3
Launois remy <remy.launois@gmail.com>
2022-05-15 13:57:37 -0400

    Initial commit

---
+++ src/BowlingTest.java
@@ -0,0 +1,22 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Project: Tp1_Bowling
+ * Package: PACKAGE_NAME
+ */
+public class BowlingTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void setPinDownFirstParameterShouldBe1Before2() {
+    }
+}
