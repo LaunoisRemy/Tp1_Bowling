72111d32aedb9ceaacdb432c0178338dd1c4c1c1
Launois remy <remy.launois@gmail.com>
2022-05-15 14:19:37 -0400

    Two test

--- src/BowlingTest.java
+++ src/BowlingTest.java
@@ -1,6 +1,8 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import stev.bowling.Game;
+import stev.bowling.NormalFrame;
 
 /**
  * Project: Tp1_Bowling
@@ -8,15 +10,26 @@ import org.junit.Test;
  */
 public class BowlingTest {
 
+    private Game game;
+
     @Before
     public void setUp() throws Exception {
+        this.game = new Game();
     }
 
     @After
     public void tearDown() throws Exception {
     }
 
+    /**
+     * Test
+     */
     @Test
     public void setPinDown_FirstParameter_1Before2() {
+        this.game.addFrame(new NormalFrame(1).setPinsDown(1, 3).setPinsDown(2, 6));
+    }
+    @Test
+    public void setPinDown_FirstParameter_2Before1_Exception() {
+        this.game.addFrame(new NormalFrame(2).setPinsDown(1, 3).setPinsDown(1, 6));
     }
 }
